cf19652f542e43412cb703d71f6674dbda03c32b
Marcelo Vasques <msouvas@gmail.com>
2018-12-11 22:18:02 -0200

    Added comments

--- lib/quanser_2DSFJ.c
+++ lib/quanser_2DSFJ.c
@@ -5,17 +5,18 @@
     \date 2018
 */
 
-#include <stdio.h>                  //Biblioteca de entrada e saída
-#include <stdlib.h>                 //Biblioteca de função padrão
-#include <poll.h>
-#include <signal.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>		//Biblioteca de entrada e saída
+#include <stdlib.h>		//Biblioteca de função padrão
+#include <poll.h>		//Biblioteca usada para interrupções.
+#include <signal.h>		//Biblioteca de sinais usada para abortar o programa.
+#include <unistd.h>		//Biblioteca usada para sleep.
+#include <fcntl.h>		//Biblioteca de manipulação de arquivos (usada nos pinos).
+#include <math.h>		//Biblioteca de funções e constantes matemáticas.
 
-#include <math.h>
+/* Header da biblioteca aqui desenvolvida */
 #include <quanser_2DSFJ.h>
 
-/*
+/*	Pinos Utilizados
 	IO0 - GPIO - END1
     IO1 - GPIO - END2
     IO2 - GPIO - DIR
@@ -44,39 +45,58 @@
 /** Valor de radianos por contagem do encoder do motor */
 #define RADS_PER_COUNT 0.0015
 
+/** Estrutura para armazenar os arquivos que configuram os pinos GPIO **/
 struct PinFiles {
+	// Sensor de fim de curso esquerdo do braço.
 	int end1;
+	// Sensor de fim de curso direito do braço.
 	int end2;
+	// Sentido do movimento do braço.
 	int dir;
+	// Reset para o decodificador de quadratura.
 	int rst;
+	// Dados lidos do decodificador de quadratura com a contagem da posição.
 	int decoder_pins[8];
 };
 
+/** Estrutura para armazenar os arquivos que configuram os pinos PWM **/
 struct PwmFiles {
 	int period;
 	int duty_cycle;
 	int enable;
 };
 
+/** Estrutura para armazenar os arquivos que configuram o pino ADC **/
 struct AdcFiles {
 	int voltage;
 	int scale;
 };
 
+/** Declaração das estruturas aqui usadas **/
 struct PinFiles gpios;
 struct PwmFiles pwm1;
 struct PwmFiles pwm2;
 struct AdcFiles dc1;
+
+/** Poll para uso de interrupções e sinal para encerrar o programa **/
 struct pollfd event_end[2];
 struct sigaction act;
 
+// Variável que indica que o programa está rodando
 static volatile int run = 1;
 
+/** \brief Encerra o programa ao receber o sinal */
 void quit(int signal) {
 	run = 0;
 }
 
+/** \brief Lê o pino e retorna seu valor */
 int getPinValue(int file) {
+	/**
+	\details Usa o pino aberto no parâmetro para ler o valor.
+	\param file Arquivo do pino a ser lido.
+	\return Valor lido do arquivo do pino.
+	*/
 	char s;
 	int value;
 
@@ -91,7 +111,14 @@ int getPinValue(int file) {
 	return value;
 }
 
+/** \brief Escreve valor no pino */
 void setPinValue(int file, int value) {
+	/**
+	\details Usa o pino aberto no parâmetro para escrever o valor passado.
+	\param file Arquivo do pino a ser escrito.
+	\param value Valor a ser escrito.
+	\return Não há retorno.
+	*/
 	char s[20];
 
 	sprintf(s, "%d", value);
@@ -100,7 +127,15 @@ void setPinValue(int file, int value) {
 	write(file, &s, sizeof s);
 }
 
+/** \brief Altera o valor de um bit do char */
 char setBit(char byte, int value, int pos) {
+	/**
+	\details Seta em 1 ou 0 um bit escolhido do byte de char.
+	\param byte Byte a ser alterado.
+	\param value Valor de bit a ser escrito.
+	\param pos Posição do byte a ter bit alterado.
+	\return Novo byte em char.
+	*/
 	if (value)
 		byte |= 1 << pos;
 	else
@@ -109,7 +144,12 @@ char setBit(char byte, int value, int pos) {
 	return byte;
 }
 
+/** \brief Lê os 8 pinos informados pelo decoder. */
 int readDecoder() {
+	/**
+	\details Lê cada pino, que representa um bit, da saída do decoder e cria um byte.
+	\return Byte formado pelos 8 pinos em um char.
+	*/
 	unsigned char data_read = 0x00;
 	int value;
 
@@ -127,7 +167,13 @@ int readDecoder() {
 	return value;
 }
 
+/** \brief Abre todos os arquivos dos pinos configurados. */
 void openPinFiles() {
+	/**
+	\details Abre e guarda nas estruturas todos os arquivos dos pinos usados no projeto.
+	\return Não há retorno.
+	*/
+
 	// END1
 	if((gpios.end1 = open("/sys/class/gpio/gpio11/value", O_RDONLY)) < 0)
 		printf("Error opening end1\n");
@@ -198,7 +244,12 @@ void openPinFiles() {
 		printf("Error opening dc1 scale\n");
 }
 
+/** \brief Converte para radianos o valor lido do decoder. */
 double getEncoderRadiansData() {
+	/**
+	\details Usa a constante do motor para converter para radianos a contagem do decoder.
+	\return Posição do braço em radianos.
+	*/
 	int count;
 
 	count = readDecoder();
@@ -206,7 +257,13 @@ double getEncoderRadiansData() {
 	return RADS_PER_COUNT * count;
 }
 
+/** \brief Usa a tensão da galileo para verificar a direção do movimento do braço. */
 int getPWMDirection(float voltage) {
+	/**
+	\details Usa a tensão da galileo para verificar a direção do movimento do braço.
+	\param voltage Tensão lógica da Galileo em DC, relativa ao PWM e o duty cycle.
+	\return O sentido do movimento em 1 ou 0.
+	*/
 	int direction;
 
 	if (voltage > 2.5f)
@@ -219,6 +276,12 @@ int getPWMDirection(float voltage) {
 
 /** \brief Seta a ciclo de trabalho do PWM */
 void setPWMDutyCycle(int file, int duty_cycle) {
+	/**
+	\details Usa o arquivo do PWM para setar o ciclo do trabalho no arquivo.
+	\param file Arquivo do PWM com o ciclo de trabalho.
+	\param duty_cycle Ciclo de trabalho em nanossegundos.
+	\return Sem retorno.
+	*/
 	char s[20];
 
 	sprintf(s, "%d", duty_cycle);
@@ -232,21 +295,19 @@ void setPWMDutyCycle(int file, int duty_cycle) {
 /** \brief Seta a tensão do motor */
 void setMotorVoltage(double value) {
     /**
-    \details Seta a tensão do motor com o valor especificado por parâmetro
-    \param value Valor do tipo inteiro usado para definir a tensão do motor
+    \details Seta a tensão do motor com o valor especificado por parâmetro.
+    \param value Valor do tipo double usado para definir a tensão do motor.
     \return Esta função não possui retorno
     */
-	int period = (1.0 / PWM_FREQ) * NANO;
+	int period = (1.0 / PWM_FREQ) * NANO; //Período é o inverso da frequência.
 
 	if (value > MOTOR_VOLTAGE)
 		value = MOTOR_VOLTAGE;
 	if (value < 0)
 		value = 0;
 
+	// Usa a fração da tensão desejada para setar o período do ciclo.
 	int cycle = (int)((value / MOTOR_VOLTAGE) * period);
-
-	//if(cycle > period*0.9)
-        //cycle = period * 0.9;
 
 	printf("Setting pwm cycle to %d, period is %d\n", cycle, period);
 	setPWMDutyCycle(pwm2.duty_cycle, cycle);
@@ -254,6 +315,11 @@ void setMotorVoltage(double value) {
 
 /** \brief Seta o período do PWM */
 void setPWMPeriod(int period) {
+	/**
+	\details Altera o período total do PWM.
+	\param period Período em nanossegundos.
+	\return Sem retorno.
+	*/
 	char s[20];
 
 	sprintf(s, "%d", period);
@@ -263,7 +329,12 @@ void setPWMPeriod(int period) {
     write(pwm2.period, s, sizeof(s));
 }
 
+/** \brief Função para checar se atingiu um fim de curso. */
 int reachedEnd() {
+	/**
+	\details Lê os dois sensores de fim de curso e informa se atingiu algum.
+	\return 1 se atingiu, 0 se não.
+	*/
 	int reached = 0;
 
 	if (getPinValue(gpios.end1))
@@ -274,7 +345,12 @@ int reachedEnd() {
 	return reached;
 }
 
+/** \brief Função para checar se atingiu o fim de curso esquerdo. */
 int endLeft() {
+	/**
+	\details Lê o sensor de fim de curso esquerdo e informa se foi atingido.
+	\return 1 se atingiu, 0 se não.
+	*/
 	int reached = 0;
 
 	if (getPinValue(gpios.end1))
@@ -283,7 +359,12 @@ int endLeft() {
 	return reached;
 }
 
+/** \brief Função para checar se atingiu o fim de curso direito. */
 int endRight() {
+	/**
+	\details Lê o sensor de fim de curso direito e informa se foi atingido.
+	\return 1 se atingiu, 0 se não.
+	*/
 	int reached = 0;
 
 	if (getPinValue(gpios.end2))
@@ -292,7 +373,13 @@ int endRight() {
 	return reached;
 }
 
+/** \brief Função que usa interrupções para checar se atingiu algum dos fins de curso. */
 void interruptionsReachedEnd() {
+	/**
+	\details Função que usa interrupções para checar se atingiu algum dos
+	fins de curso, ficando travado enquanto não atinge.
+	\return Sem retorno.
+	*/
 	unsigned char c;
 
 	event_end[0].fd = gpios.end1;
@@ -323,6 +410,7 @@ double pid(double dt, double *P_error, double *I_error, double *D_error, double 
     return 0.1 * *P_error + 0.2 * *I_error + 0.0005 * *D_error;
 }
 
+/** \brief  Inicializa os pinos com valores seguros. */
 void initialize() {
 	openPinFiles();
 	setPWMPeriod((1.0 / PWM_FREQ) * NANO);
